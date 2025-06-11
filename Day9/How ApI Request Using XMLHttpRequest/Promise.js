2da40b366bd9d2b0609297f0f52de7f95c004dee
Angad Bari <angadbari2@gmail.com>
2025-06-11 10:18:34 +0530

    UnderStanding try-Catch,Promise,Fetch

--- Day9/How ApI Request Using XMLHttpRequest/Promise.js
+++ Day9/How ApI Request Using XMLHttpRequest/Promise.js
@@ -1,23 +1,27 @@
 
 //+++++++++++++++++++Promise:- ++++++++++++
-// You don't have to complete the task immediately — you just say, "I'm working on it, and I'll give you the result later."
 
-//Three states of a promise:
+//1.Promise -->Object
+//2.You don't have to complete the task immediately — you just say, "I'm working on it, and I'll give you the result later."
+//3.Three states of a promise:
                          // 1. Pending: The initial state, neither fulfilled nor rejected.
                          // 2. Fulfilled: The operation completed successfully.
                          // 3. Rejected: The operation failed.
 
 
-//++++When Promise not Avliable++
+//++++++++++When Promise not Avliable++++++
+
 //That default use async and await
 //Best Libary:-Q And BlueBirds(That time Use)
 
 
 
 //++++++Creation Of Promise(Diffrent Way:-)+++++++++++++
+
 //1.Comsume Or 2.Create Promise
 
-//+++++++++++++1 way++++++++++
+
+//+++++++++++++ 1 way ++++++++++
 
 //2.Create Promise
 const promiseOne= new Promise(function(resolve,reject){
@@ -25,7 +29,7 @@ const promiseOne= new Promise(function(resolve,reject){
     //DB Calss,cryptography,netwok
 
     setTimeout(function(){
-        // console.log("Async  task is Compelete")
+         console.log("Async  task is Compelete")
         resolve()//when this function call then .then return value,also you give data also..
     },1000)
 }) 
@@ -33,23 +37,23 @@ const promiseOne= new Promise(function(resolve,reject){
 //1.Consume Promise
 promiseOne.then(function(){
     //return the value
-    // console.log("promise consumed")
+     console.log("promise consumed")
 
 })
 
 
-//+++++++++++++++++2 way++++
+//+++++++++++++++++ 2 Way +++++++++++++++++
 
 new Promise(function(resolve,reject){
     setTimeout(()=>{
-        // console.log("Async task 2")
+         console.log("Async task 2")
         resolve()
     },1000)
 }).then(()=>{
-    // console.log("Promise2 Consumed..")
+    console.log("Promise2 Consumed..")
 })
 
-//+++++++++3 way++++++++
+//+++++++++++++++++++ 3 Way +++++++++++++++
 
 const PromiseThree=new Promise((resolve,reject)=>{
   setTimeout(()=>{
@@ -61,7 +65,7 @@ PromiseThree.then((user)=>{
    console.log(user)
 })
 
-//+++++++++++ 4 Way+++++++++
+//++++++++++++++++++++ 4 Way ++++++++++++++++++++++++++++
 
 const promiseFour= new Promise((resolve,reject)=>{
    setTimeout(()=>{
@@ -92,3 +96,53 @@ promiseFour
 .finally(()=>{
     console.log("The Promise is either reslove or reject...")
 })//allways excute
+
+
+//++++++++++++++++++++ 5 Way +++++++++++++++++++++
+
+const PromiseFive=new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      let error = true;
+      if (!error) {
+        resolve({ username: "JavaScript", password: "123" });
+      } else {
+        reject("ERROR:Somthing went wrong");
+      }
+    }, 1000);
+})
+
+async function consumePromiseFive(){
+    try {
+        const response=await PromiseFive
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+consumePromiseFive()
+
+//++++++++++++++++++=====+++++++++++++
+
+async function getAllUser(){
+ try {
+     const response =await fetch("https://api.github.com/users/AngadBari");
+     const data=await response.json()
+     console.log(data)
+ } catch (error) {
+    console.log(error)
+ }
+}
+// getAllUser();
+
+//++++++++Above Code Using .then and .Catch+++++++
+
+fetch("https://api.github.com/users/AngadBari")
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+   console.log(data)
+})
+.catch((error)=>{
+   console.log("ERROR",error)
+})
